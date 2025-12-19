af832df94ef0f3b8aca6f10538b1dd528b7d2af7
Madhan R <madhanravi@gmail.com>
2025-12-19 12:45:34 +0530

    Base Setup, Tailwind, DaisyUI

---
+++ devTinder-web/src/App.jsx
@@ -0,0 +1,49 @@
+import logo from "./assets/Logo.png";
+
+function App() {
+  return (
+    <>
+      <div className="navbar bg-base-300 shadow-sm">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">
+            <img src={logo} alt="Logo" className="w-14" />
+            DevIinder
+          </a>
+        </div>
+        <div className="flex gap-2">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar mx-5">
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://www.shutterstock.com/image-vector/deadpool-icon-logo-sign-symbol-600nw-2394182337.jpg"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
